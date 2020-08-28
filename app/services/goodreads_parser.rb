1a28820a46594763db27ccce93153cb71d2ee924
NicolasDontigny <nicolas.dontigny1@gmail.com>
2020-08-27 21:51:48 -0400

    Fixed goodreads_parser

--- app/services/goodreads_parser.rb
+++ app/services/goodreads_parser.rb
@@ -8,48 +8,15 @@ class GoodreadsParser
 
   def execute
     html_file = open(@url).read
-    html_doc = Nokogiri::HTML(html_file)
+    @html_doc = Nokogiri::HTML(html_file)
 
-    @title = html_doc.at('h1#bookTitle').text.strip
-
-    @publishing_date = nil
-    html_doc.search('#details .row').each do |element|
-      if element.text.strip.match?('Published')
-        year_pattern = /Published(\\n)?\s*[a-zA-Z]+\s\d{1,2}[a-zA-Z]{1,2}\s(?<year>\d{4}(\\n)?\s*by)/
-        match_data = element.text.strip.match(year_pattern)
-        @publishing_date = match_data.nil? ? nil : match_data[:year].to_i
-      end
-    end
-
-    rating_element = html_doc.at('#bookMeta [itemprop="ratingValue"]')
-    @rating = rating_element.text.strip.to_f
-
-    puts "====================="
-    puts "rating: #{@rating.inspect}"
-    puts "====================="
-
-    number_of_reviews_element = html_doc.at('#bookMeta [itemprop="ratingCount"]')
-    @number_of_reviews = number_of_reviews_element.attribute("content").value.to_i
-
-    @authors = []
-    html_doc.search('#bookAuthors .authorName__container').each do |element|
-      names = element.at('[itemprop="name"]').text.strip.split(/\s/)
-      if names.count > 2
-        first_name = names.shift
-        last_name = names.join(' ')
-      else
-        first_name = names[0] || ''
-        last_name = names[1] || ''
-      end
-
-      existing_author = Author.find_by(first_name: first_name, last_name: last_name)
-      author = existing_author || Author.create(first_name: first_name, last_name: last_name)
-      @authors << author
-    end
-
-    @description = html_doc.at('#description span:nth-child(2)').inner_html
-
-    @cover_url = html_doc.at('#coverImage').attribute('src').value
+    set_title
+    set_publishing_date
+    set_rating
+    set_number_of_reviews
+    set_authors
+    set_description
+    set_cover_url
 
     @book = Book.new(
       title: @title,
@@ -65,5 +32,67 @@ class GoodreadsParser
     )
 
     @book
+  end
+
+  private
+
+  def set_title
+    @title = @html_doc.at('h1#bookTitle').text.strip
+  end
+
+  def set_publishing_date
+    @publishing_date = nil
+    @html_doc.search('#details .row').each do |element|
+      next unless element.text.strip.match?('Published')
+
+      year_pattern = /Published(\\n)?\s*[a-zA-Z]+\s\d{1,2}[a-zA-Z]{1,2}\s(?<year>\d{4}(\\n)?\s*by)/
+      match_data = element.text.strip.match(year_pattern)
+      @publishing_date = match_data.nil? ? nil : match_data[:year].to_i
+    end
+  end
+
+  def set_rating
+    rating_element = @html_doc.at('#bookMeta [itemprop="ratingValue"]')
+    return unless rating_element
+
+    @rating = rating_element.text.strip.to_f
+  end
+
+  def set_number_of_reviews
+    number_of_reviews_element = @html_doc.at('#bookMeta [itemprop="ratingCount"]')
+    return unless number_of_reviews_element
+
+    @number_of_reviews = number_of_reviews_element
+                         .attribute('content').value.to_i
+  end
+
+  def set_authors
+    @authors = []
+    @html_doc.search('#bookAuthors .authorName__container').each do |element|
+      names = element.at('[itemprop="name"]').text.strip.split(/\s/)
+      next unless names
+
+      if names.count > 2
+        first_name = names.shift
+        last_name = names.join(' ')
+      else
+        first_name = names[0] || ''
+        last_name = names[1] || ''
+      end
+
+      existing_author = Author
+                        .find_by(first_name: first_name, last_name: last_name)
+      author = existing_author ||
+               Author.create(first_name: first_name, last_name: last_name)
+      @authors << author
+    end
+  end
+
+  def set_description
+    @description = @html_doc.at('#description span:nth-child(2)')&.inner_html
+  end
+
+  def set_cover_url
+    @cover_url = @html_doc.at('#coverImage')&.attribute('src')&.value
   end
 end
